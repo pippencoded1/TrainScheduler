e990b65f9dfe6ee3323de790da177abcc6614217
Marissa Pippen <marissacpippen@gmail.com>
2018-03-24 17:40:47 -0400

    added firbase

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,15 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBWJNBn5xawr18zi-6egSVLmNS__qE-CuU",
+    authDomain: "trainschedulerproject-f87eb.firebaseapp.com",
+    databaseURL: "https://trainschedulerproject-f87eb.firebaseio.com",
+    projectId: "trainschedulerproject-f87eb",
+    storageBucket: "",
+    messagingSenderId: "337644337263"
+};
+firebase.initializeApp(config);
+
+
 $(document).ready({
 
 
