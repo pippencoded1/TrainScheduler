41a101a0a5e90a476e7ceb17a61bfbea7e974893
Marissa Pippen <marissacpippen@gmail.com>
2018-03-31 09:40:47 -0400

    Final Push

--- assets/app.js
+++ assets/app.js
@@ -9,18 +9,47 @@ var config = {
 };
 firebase.initializeApp(config);
 
-
-$(document).ready({
-
-
+// Collection Messages
+var messagesRef = firebase.database().ref('messages');
 
 
+    // Listener for form submit
+
+document.getElementById('TrainForm').addEventListener('submit', submitForm);
+
+function submitForm(e){
+    e.preventDefault();
+   
+    var trainname= getInputVal('trainname');
+    var destination = getInputVal('destination');
+    var firstarrival = getInputVal('firstarrival');
+    var frequency = getInputVal('frequency');
+    
+    saveMessage(trainname, destination, firstarrival, frequency);
+
+    // Clear input after submit
+    document.getElementById('TrainForm').reset();
+}
 
 
+// Get Form values'
+function getInputVal(id){
+    return document.getElementById(id).value;
+}
 
 
-});
+// Save messages to Firebase
 
+function saveMessage(trainname, destination, firstarrival, frequency){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        trainname: trainname,
+        destination: destination,
+        firstarrival: firstarrival,
+        frequency: frequency,
+
+    })
+}
 
 
 
